75acbaef31b3e73d74cb93a7c4c692a744ce7b4d
tsv129 <32687515+tsv129@users.noreply.github.com>
2018-05-04 01:08:47 +0300

    Vesion 1.0

---
+++ Samos/Form1.Designer.cs
@@ -0,0 +1,253 @@
+﻿namespace Samos
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.User = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Read_File = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Write_File = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Rename_File = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Delete_File = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Move_File = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Copy_File = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.User,
+            this.Read_File,
+            this.Write_File,
+            this.Rename_File,
+            this.Delete_File,
+            this.Move_File,
+            this.Copy_File});
+            this.dataGridView1.Location = new System.Drawing.Point(231, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.RowHeadersWidth = 35;
+            this.dataGridView1.RowTemplate.Height = 20;
+            this.dataGridView1.Size = new System.Drawing.Size(528, 303);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // User
+            // 
+            this.User.HeaderText = "User";
+            this.User.Name = "User";
+            this.User.ReadOnly = true;
+            this.User.Width = 75;
+            // 
+            // Read_File
+            // 
+            this.Read_File.HeaderText = "Read_File";
+            this.Read_File.Name = "Read_File";
+            this.Read_File.ReadOnly = true;
+            this.Read_File.Width = 75;
+            // 
+            // Write_File
+            // 
+            this.Write_File.HeaderText = "Write_File";
+            this.Write_File.Name = "Write_File";
+            this.Write_File.ReadOnly = true;
+            this.Write_File.Width = 75;
+            // 
+            // Rename_File
+            // 
+            this.Rename_File.HeaderText = "Rename_File";
+            this.Rename_File.Name = "Rename_File";
+            this.Rename_File.ReadOnly = true;
+            this.Rename_File.Width = 75;
+            // 
+            // Delete_File
+            // 
+            this.Delete_File.HeaderText = "Delete_File";
+            this.Delete_File.Name = "Delete_File";
+            this.Delete_File.ReadOnly = true;
+            this.Delete_File.Width = 75;
+            // 
+            // Move_File
+            // 
+            this.Move_File.HeaderText = "Move_File";
+            this.Move_File.Name = "Move_File";
+            this.Move_File.ReadOnly = true;
+            this.Move_File.Width = 75;
+            // 
+            // Copy_File
+            // 
+            this.Copy_File.HeaderText = "Copy_File";
+            this.Copy_File.Name = "Copy_File";
+            this.Copy_File.ReadOnly = true;
+            this.Copy_File.Width = 75;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(111, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(104, 46);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Сгенирировать права";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 34);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(84, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(38, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Имя:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 120);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.Text = "Read File";
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(118, 120);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 5;
+            this.textBox3.Text = "Write File";
+            this.textBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(118, 170);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.ReadOnly = true;
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 7;
+            this.textBox4.Text = "Delete File";
+            this.textBox4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(12, 170);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.ReadOnly = true;
+            this.textBox5.Size = new System.Drawing.Size(100, 20);
+            this.textBox5.TabIndex = 6;
+            this.textBox5.Text = "Rename File";
+            this.textBox5.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(118, 221);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.ReadOnly = true;
+            this.textBox6.Size = new System.Drawing.Size(100, 20);
+            this.textBox6.TabIndex = 9;
+            this.textBox6.Text = "Copy File";
+            this.textBox6.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(12, 221);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.ReadOnly = true;
+            this.textBox7.Size = new System.Drawing.Size(100, 20);
+            this.textBox7.TabIndex = 8;
+            this.textBox7.Text = "Move File";
+            this.textBox7.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(771, 334);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "FormMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "ТЗИ";
+            this.Load += new System.EventHandler(this.FormMain_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn User;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Read_File;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Write_File;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Rename_File;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Delete_File;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Move_File;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Copy_File;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+    }
+}
+
---
+++ Samos/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Samos
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        int rid;
+        Random random = new Random();
+        TextBox[] tbarr = new TextBox[6];
+
+        private void Random_access(string name)
+        {
+            dataGridView1[0, rid].Value = name;
+
+            for (int i=1; i < 7; i++)
+                dataGridView1[i, rid].Value = "False";
+
+            if (random.Next(2) == 1)
+            {
+                dataGridView1[1, rid].Value = "True";
+                if (random.Next(2) == 1)
+                {
+                    dataGridView1[2, rid].Value = "True";
+                }
+            }
+
+            for (int i=3; i < 7; i++)
+            {
+                if (random.Next(2) == 1)
+                    dataGridView1[i, rid].Value = "True";
+            }
+
+            Color_cell();
+
+            rid++;
+        }
+
+        private void Expand_rows()
+        {
+            for (int i = 0; i < 15; i++)
+                dataGridView1.Rows.Add();
+        }
+
+        private void Color_cell()
+        {
+            for (int i = 1; i < 7; i++)
+                if (dataGridView1[i, rid].Value.ToString() == "True")
+                {
+                    dataGridView1[i, rid].Style.BackColor = Color.Green;
+                    tbarr[i-1].BackColor = Color.Green;
+                }
+                else
+                {
+                    dataGridView1[i, rid].Style.BackColor = Color.Red;
+                    tbarr[i - 1].BackColor = Color.Red;
+                }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (rid % 15 == 0)
+                Expand_rows();
+            Random_access(textBox1.Text);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            dataGridView1.Rows.Add();
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            dataGridView1.Rows.Add();
+        }
+
+        private void FormMain_Load(object sender, EventArgs e)
+        {
+            tbarr[0] = textBox2;
+            tbarr[1] = textBox3;
+            tbarr[2] = textBox4;
+            tbarr[3] = textBox5;
+            tbarr[4] = textBox6;
+            tbarr[5] = textBox7;
+        }
+    }
+}
